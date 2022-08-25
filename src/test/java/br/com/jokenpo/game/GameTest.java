db08c409cf67e00e10000f5bad1e0921f43d47d7
Kalebe Tavares <kalebe.tavares@gmail.com>
2022-08-25 10:30:18 -0300

    featureApplication created from this test

---
+++ src/test/java/br/com/jokenpo/game/GameTest.java
@@ -0,0 +1,24 @@
+package br.com.jokenpo.game;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class GameTest {
+
+    @Test
+    public void ShouldStartGame(){
+        startGame();
+    }
+
+    private void toBattle() {
+    }
+
+    private void endGame() {
+
+    }
+
+    private void startGame() {
+
+    }
+}
