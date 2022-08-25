b561861f7fcc784c0197bf2da0c20bff018dcacb
Kalebe Tavares <kalebe.tavares@gmail.com>
2022-08-25 10:25:13 -0300

    feature:Creation of business rules within the GameService Class

---
+++ src/main/java/br/com/jokenpo/game/GameService.java
@@ -0,0 +1,49 @@
+package br.com.jokenpo.game;
+
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class GameService implements Comparator<Card>{
+    private Scanner input = new Scanner(System.in);
+    private Deck cardsPlayer1 = new Deck();
+    private Deck cardsPlayer2 = new Deck();
+
+    public void startGame() {
+        System.out.println("Do you want to play solo or two players?");
+        Integer numberOfPlayers = this.input.nextInt();
+        if(numberOfPlayers == 2) {
+            toBattle();
+        }
+    }
+
+    private void toBattle() {
+        System.out.println("Which symbol do you want to play?\n1-Paper 2-Stone 3-Scissor");
+        Integer numbercard1 = input.nextInt();
+        Integer numberCard2 = input.nextInt();
+
+        cardsPlayer1.playCard(numbercard1);
+        cardsPlayer2.playCard(numberCard2);
+
+        battleOfCards(cardsPlayer1.getCard(), cardsPlayer2.getCard());
+    }
+
+    private void battleOfCards(Card card1, Card card2) {
+        Integer result = compare(card1, card2);
+        if(card1.equals(card2)) System.out.println("Players drew!");
+        else if (result == 1) {
+            System.out.println("Player 1 win!");
+        } else {
+            System.out.println("Player 2 win!");
+        }
+    }
+
+    @Override
+    public int compare(Card c1, Card c2) {
+        if(c1.equals(Card.PAPER) && c2.equals(Card.STONE) ||
+                c1.equals(Card.STONE) && c2.equals(Card.SCISSOR) ||
+                c1.equals(Card.SCISSOR) && c2.equals(Card.PAPER)){
+            return 1;
+        }
+        return 0;
+    }
+}
