ee035888626b476df98c126b9c7613b726ec6a46
Kalebe Tavares <kalebe.tavares@gmail.com>
2022-08-25 09:12:48 -0300

    feature:Creation of the enum for possible plays

---
+++ src/main/java/br/com/jokenpo/game/Card.java
@@ -0,0 +1,14 @@
+package br.com.jokenpo.game;
+
+public enum Card {
+    PAPER(1), STONE(2), SCISSOR(3);
+
+    private Integer value;
+    Card(Integer value) {
+     this.value = value;
+    }
+
+    public Integer getValue() {
+        return this.value;
+    }
+}
